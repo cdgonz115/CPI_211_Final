f4e3609a1aa6ec8fff6537fa6260d33990dbc784
propredator23 <christiandash@hotmail.com>
2020-04-13 15:32:55 -0700

    Future Level
    Most of the Future Level done

---
+++ Assets/_Scripts/Future/DroneCrash.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneCrash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject crashLocation;
+
+    public void OnEnable()
+    {
+        //crash();
+    }
+    void Update()
+    {
+        if(transform.position!=crashLocation.transform.position)transform.position+=(transform.position - crashLocation.transform.position)*-.02f;
+    }
+}
