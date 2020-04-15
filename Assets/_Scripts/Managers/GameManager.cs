bb91f6ee93342f620430d22a93e5a831556892fe
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-04-14 18:52:11 -0700

    Game over scene and prep

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -24,15 +24,36 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void ChangeScene(string sceneName)
+    /// <summary>
+    /// Method that changes to a inputted scene and optionally saves
+    /// the scene we are going into
+    /// </summary>
+    /// <param name="sceneName">The name of the scene to change to</param>
+    /// <param name="saveScene">Set to true to save the scene. This is true by default</param>
+    public void SetLevel(string sceneName, bool saveScene = true)
     {
         Scene nextScene = SceneManager.GetSceneByName(sceneName);
 
         if (nextScene != null)
         {
-            CurrentScene = nextScene;
+            if (saveScene)
+            {
+                CurrentScene = nextScene;
+            }
 
             SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    /// <summary>
+    /// Method meant to be called by UI that will allow the player
+    /// to retry a level. Will mostly be used in the game over scene
+    /// </summary>
+    public void RetryLevel()
+    {
+        if(CurrentScene != null)
+        {
+            SceneManager.LoadScene(CurrentScene.name);
         }
     }
 }
