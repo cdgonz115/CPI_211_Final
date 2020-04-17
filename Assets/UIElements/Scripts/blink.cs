749c2593c848e36fbd6697807ab4b886ed1b9ffe
Agustin Gomez <gusmandoman@gmail.com>
2020-04-16 19:52:11 -0700

    Main Menu Animation

---
+++ Assets/UIElements/Scripts/blink.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blink : MonoBehaviour
+{
+    public movingPlatform top;
+    public movingPlatform bottom;
+
+    
+    private void Update() 
+    {
+        if(Input.GetMouseButtonDown(0) && !top.yMovement && !bottom.yMovement)
+        {
+            top.yMovement = true;
+            bottom.yMovement = true;
+        }
+
+        if(top.oneRotation && bottom.oneRotation)
+            {
+                top.yMovement = false;
+                bottom.yMovement = false;
+
+                top.oneRotation = false;
+                bottom.oneRotation = false;
+            }
+    }
+
+    
+    
+}
