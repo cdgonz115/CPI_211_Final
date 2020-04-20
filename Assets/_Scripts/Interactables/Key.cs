b99e5f8e7234b8f262568a7d3c565bc2a5062eef
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-04-19 17:35:41 -0700

    Player can now exit future after getting key

--- Assets/_Scripts/Interactables/Key.cs
+++ Assets/_Scripts/Interactables/Key.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Key : Interactable
 {
+    public GameObject SceneTransition;
+
     protected override void PerformAction()
     {
         base.PerformAction();
 
+        SceneTransition.SetActive(true);
         Player.KeyCount++;
     }
 }
