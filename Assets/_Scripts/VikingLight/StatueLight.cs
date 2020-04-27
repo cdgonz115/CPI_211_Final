a39215eb5da3f08c51e9ace5482a80b350ec6334
propredator23 <christiandash@hotmail.com>
2020-04-27 15:15:43 -0700

    Map3
    Put the base things for the viking map still alot of detail still needs to be added

---
+++ Assets/_Scripts/VikingLight/StatueLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatueLight : MonoBehaviour
+{
+    public GameObject statue1;    
+    public GameObject statue2;    
+    public GameObject statue3;
+
+    private void Update()
+    {
+        print(Vector3.Distance(transform.position, statue1.transform.position));
+        closestStatue();
+    }
+    private void closestStatue()
+    {
+        float shortest = Vector3.Distance(transform.position,statue1.transform.position);
+        float temp = Vector3.Distance(transform.position, statue2.transform.position);
+        if (temp < shortest) shortest = temp;
+        temp = Vector3.Distance(transform.position, statue3.transform.position);
+        if (temp < shortest) shortest = Vector3.Distance(transform.position, statue3.transform.position);
+    }
+}
+
