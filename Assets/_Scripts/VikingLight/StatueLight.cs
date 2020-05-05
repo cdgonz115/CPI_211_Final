40a1e6890002690385250ef2d95b900f35ee833b
propredator23 <christiandash@hotmail.com>
2020-05-04 18:55:05 -0700

    Light
    Added the Script that shows the statues no matter where you are in the map as well as updated som things about the maps

--- Assets/_Scripts/VikingLight/StatueLight.cs
+++ Assets/_Scripts/VikingLight/StatueLight.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class StatueLight : MonoBehaviour
 {
-    public GameObject statue1;    
-    public GameObject statue2;    
-    public GameObject statue3;
+    public GameObject[] statues;
+    public GameObject lightPrefab;
+    private GameObject SLight;
+    private int closest;
 
+    private void Start()
+    {
+        SLight = Instantiate(lightPrefab, transform.position, transform.rotation);
+    }
     private void Update()
     {
-        print(Vector3.Distance(transform.position, statue1.transform.position));
-        closestStatue();
+        float dis=closestStatue();
+        SLight.transform.position = transform.position - (transform.position - statues[closest].transform.position) * .007f;
+        if(dis <= 500f) SLight.GetComponent<Light>().intensity= dis / 500f;
     }
-    private void closestStatue()
+    private float closestStatue()
     {
-        float shortest = Vector3.Distance(transform.position,statue1.transform.position);
-        float temp = Vector3.Distance(transform.position, statue2.transform.position);
-        if (temp < shortest) shortest = temp;
-        temp = Vector3.Distance(transform.position, statue3.transform.position);
-        if (temp < shortest) shortest = Vector3.Distance(transform.position, statue3.transform.position);
+        float shortest = Vector3.Distance(transform.position, statues[0].transform.position);
+        float temp = Vector3.Distance(transform.position, statues[1].transform.position);
+        closest = 0;
+        if (temp < shortest)
+        {
+            shortest = temp;
+            closest = 1;
+        }
+        temp = Vector3.Distance(transform.position, statues[2].transform.position);
+        if (temp < shortest)
+        {
+            shortest = temp;
+            closest = 2;
+        }
+        return shortest;
     }
 }
 
