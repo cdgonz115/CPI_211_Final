357c117b92bc9bc642f5a306434fca52aa5dfd80
Nick N <nicholas.ngoc.nguyen@gmail.com>
2020-05-06 15:02:20 -0700

    Wired up the main menu

---
+++ Assets/_Scripts/MainMenuHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Simple class that handles the Main Menu scene.
+/// Mostly the UI button press methods
+/// </summary>
+public class MainMenuHandler : MonoBehaviour
+{
+    public void PressPlay()
+    {
+        SceneManager.LoadScene("Museum");
+    }
+
+    public void PressQuit()
+    {
+        Application.Quit();
+    }
+}
