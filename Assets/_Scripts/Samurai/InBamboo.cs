0080c7e8e3c1151a200e6f5bf245cf9903f4c66e
propredator23 <christiandash@hotmail.com>
2020-05-05 20:48:35 -0700

    Samurai
    Updated the dude by the bamboo so it goes into it when the player sees him, also redid the nevmesh

---
+++ Assets/_Scripts/Samurai/InBamboo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InBamboo : MonoBehaviour
+{
+    public GameObject npc;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            npc.GetComponent<NPCscriptedMove>().enabled = true;
+        }
+    }
+}
